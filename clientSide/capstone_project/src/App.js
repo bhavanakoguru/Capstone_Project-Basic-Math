b9fea56ee8d704095593530ece46f6f7f3282bd3
bhavanakoguru <kogurubhavana@gmail.com>
2023-06-10 02:07:44 +0530

    Static View Page Completed

--- clientSide/capstone_project/src/App.js
+++ clientSide/capstone_project/src/App.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
-import './App.css';
-import DisplaySignup from './components/Signup';
-import DisplaySignin from './components/Signin';
-import DisplayForgot from './components/Forgotpassword';
-import DisplayForgotUser from './components/ForgotUsername';
-import Displayhome from './components/Homepage';
-
+import React from 'react';
+// import React, { useState } from 'react';
+// import './App.css';
+// import DisplaySignup from './components/Signup';
+// import DisplaySignin from './components/Signin';
+// import DisplayForgot from './components/Forgotpassword';
+// import DisplayForgotUser from './components/ForgotUsername';
+// import Displayhome from './components/Homepage';
+  import DisplayViewpage from './components/ViewMeterial';
 function App() {
-  const [islogin, setIslogin] = useState(false);
-  const [forgotpassword, setForgotpassword] = useState(false);
-  const [forgotusername, setForgotusername] = useState(false);
-  const [homepage,setHomepage] = useState(false);
+  // const [islogin, setIslogin] = useState(false);
+  // const [forgotpassword, setForgotpassword] = useState(false);
+  // const [forgotusername, setForgotusername] = useState(false);
+  // const [homepage,setHomepage] = useState(false);
   return (
     <div className="App">
       {
-        (islogin) ? (!forgotpassword && !forgotusername) ? (homepage) ? <Displayhome /> : <DisplaySignin islogin={islogin} setIslogin={setIslogin} setForgotpassword={setForgotpassword} setForgotusername={setForgotusername} setHomepage={setHomepage}/> : (forgotpassword) ? <DisplayForgot setForgotpassword={setForgotpassword} /> : <DisplayForgotUser setForgotusername={setForgotusername} /> : <DisplaySignup islogin={islogin} setIslogin={setIslogin} />
+        // (islogin) ? (!forgotpassword && !forgotusername) ? (homepage) ? <Displayhome /> : <DisplaySignin islogin={islogin} setIslogin={setIslogin} setForgotpassword={setForgotpassword} setForgotusername={setForgotusername} setHomepage={setHomepage}/> : (forgotpassword) ? <DisplayForgot setForgotpassword={setForgotpassword} /> : <DisplayForgotUser setForgotusername={setForgotusername} /> : <DisplaySignup islogin={islogin} setIslogin={setIslogin} />
+        <DisplayViewpage></DisplayViewpage>
       }
     </div>
   );
