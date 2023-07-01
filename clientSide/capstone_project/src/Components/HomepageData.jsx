39b77cc48a2c409bd61a9fe534edeef3959c01a8
bhavanakoguru <kogurubhavana@gmail.com>
2023-07-02 03:26:18 +0530

    displayin data

--- clientSide/capstone_project/src/Components/HomepageData.jsx
+++ clientSide/capstone_project/src/Components/HomepageData.jsx
@@ -3,11 +3,40 @@ import "./Homepage.css";
 
 function ExecuteHomePage() {
     const [course, setCourse] = useState([]);
-    const [units,setUnits]=useState([]);
-    const [Subunits,SetSubunits]=useState([]);
+    const [units, setUnits] = useState([]);
+    const [Subunits, setSubunits] = useState([]);
+    const [SubunitsInfo, SetSubunitsInfo] = useState([]);
+    //const [open, setOpen] = useState(false);
+    //const [isSelected, setIsSelected] = useState(-1);
+    //const [active,setActive] = useState(false);
+
+    // const toggleDropdown = () => {
+    //     setActive(!active)
+    // }
+
+    // const handleClick = (i) => {
+    //     setIsSelected(i)
+    // }
+
+    // const renderOptions = () => {
+    //     if (!this.props.options) {
+    //         return;
+    //     }
+
+    //     return this.props.options.map((option, i) => {
+    //         return (
+    //             <li
+    //                 onClick={evt => this.handleClick(i)}
+    //                 key={i}
+    //                 className={"dropdown__list-item " + (i === this.state.selected ? 'dropdown__list-item--active' : '')}
+    //             >
+    //                 {option}
+    //             </li>
+    //         );
+    //     });
+    // }
 
     function Go() {
-        console.log("start")
         fetch(`http://localhost:5000/course`)
             .then(response => response.json())
             .then(data => {
@@ -16,26 +45,32 @@ function ExecuteHomePage() {
                 //course.append(data[i].course_Name);
             })
     }
-    function Click() {
-        console.log("start")
+    function Click() 
+    {
         fetch(`http://localhost:5000/units`)
             .then(response => response.json())
             .then(unitdata => {
-                console.log(unitdata);
-                console.log("Bhavana");
                 setUnits(unitdata)
             })
     }
     function ClickMe() {
-        console.log("start")
+        console.log("start");
         fetch(`http://localhost:5000/Subunits`)
             .then(response => response.json())
             .then(subunitdata => {
                 console.log(subunitdata)
-                SetSubunits(subunitdata)
+                // SetSubunits(subunitdata)
             })
     }
-    //go()
+    function ClickOnce(){
+        console.log("start");
+        fetch(`http://localhost:5000/Subunits`)
+            .then(response => response.json())
+            .then(subunitcontent => {
+                console.log(subunitcontent)
+                SetSubunitsInfo(subunitcontent)
+            })
+    }
     return (
         <div>
             <div className="main">
@@ -52,54 +87,84 @@ function ExecuteHomePage() {
                             <p id="mainpara">With Kalvium, learn computer science with a world-class curriculum designed with the best CXOs of the tech world.</p>
                         </div>
                         <div>
-                            <img src="/resources/livebooksheroimgkalvium.svg" alt="image1" className="livebooeo" />
+                            <img src="/resources/livebooksheroimgkalvium.svg" alt="image1" className="Livebookhero" />
                         </div>
                     </div>
                     <div className="displaycontent">
-                    <div className="mid">
-                        <h2 className="courseinfo">
+                        <div className="mid">
+                            <h2 className="courseinfo">
+                                {
+                                    course.map(({ course_Name }) => <div>
+                                        <h1 className='course' onClick={() => Click()}>
+                                            {course_Name}
+                                        </h1>
+                                    </div>)
+                                }
+                            </h2>
+                        </div>
+                        <div className="unitsborder">
                             {
-                                course.map(({ course_Name }) => <div>
-                                    <h1 className='course' onClick={()=>Click()}>
-                                        {course_Name}
-                                    </h1>
-                                </div>)
+                                units.map(({ unit_Name,Unit_Content }) =>
+                                    <div className="combo">
+                                        <div className='units'>
+                                            <div className="rambus">
+                                                <img src="/resources/rambusimg.png" alt="" className="a" />
+                                            </div>
+                                            <div className="text">
+                                                <h2 className="c" onClick={() => ClickMe()}>
+                                                    {unit_Name}
+                                                    {/* <p>{Unit_Content}</p><br></br> */}
+                                                    {/* <div className="unitInfo">
+                                                        {
+                                                            Subunits.map(({ subUnit_Name, subUnit_Content }) => <div>
+                                                                <div className='Subunits' >
+                                                                    <div className="circle"></div><p>{subUnit_Name}</p>
+                                                                </div>
+                                                                <p className="fontweight">{subUnit_Content}</p>
+                                                            </div>)
+                                                        }
+                                                    </div> */}
+                                                </h2>
+                                            </div>
+                                            <div className="dropdown">
+                                                <img src="/resources/downarrow.png" alt="" className="b" />
+                                            </div>
+                                        </div>
+                                    </div>)
                             }
-                        </h2>
-                    </div>
-                    <div>
+                        </div>
+                        {/* <div className="unitInfo">
                             {
-                                units.map(({ unit_Name }) => <div>
-                                    <h1 className='units' onClick={()=>Click()}>
-                                        {unit_Name}
-                                        <br></br>
-                                    </h1>
-                                </div>)
-                            }
-                    </div><br></br>
-                    {/* <div className="unitInfo">
-                            {
-                                Subunits.map(({ subUnit_Name,subUnit_Content }) => <div>
-                                    <h1 className='Subunits' onClick={()=>ClickMe()}>
+                                Subunits.map(({ subUnit_Name, subUnit_Content }) => <div>
+                                    <h1 className='Subunits' >
                                         {subUnit_Name}
-                                        {subUnit_Content}
-                                        <p>bhavana</p>
+                                       
                                     </h1>
                                 </div>)
                             }
-                    </div> */}
+                        </div> */}
                     </div>
                     <div className="unitInfo">
                             {
-                                Subunits.map(({ subUnit_Name,subUnit_Content }) => <div>
-                                    <h1 className='Subunits' onClick={()=>ClickMe()}>
-                                        {subUnit_Name}
-                                        {subUnit_Content}
-                                        <p>bhavana</p>
-                                    </h1>
+                                Subunits.map(({ subUnit_Name, subUnit_Content }) => <div>
+                                    <div className='Subunits' >
+                                        <div className="circle"></div><p>{subUnit_Name}</p>
+                                    </div>
+                                    {/* <p className="fontweight">{subUnit_Content}</p> */}
                                 </div>)
                             }
-                    </div>   
+                    </div>
+                    <div className="subtopicInfo">
+                            {
+                                SubunitsInfo.map(({subUnit_Content }) => <div>
+                                    <div className='SubunitsInfo' >
+                                        <div className="circle"></div><p>{subUnit_Content}</p>
+                                    </div>
+                                    
+                                </div>)
+                            }
+                    </div>
+                    
                 </div>
             </div>
         </div>
